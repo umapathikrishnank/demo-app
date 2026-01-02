691c46b5b66b9f05f9818147727190dc41565d81
Umapathi <umapathi.krishnan@nulogic.io>
2026-01-02 16:10:27 +0530

    code update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+  }, []);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      Hello Next JS {process.env.NEXT_PUBLIC_API_URL }
